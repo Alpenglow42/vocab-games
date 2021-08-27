2f1e93b45ff9030775f306d0f1d2da604b0ae283
Alpenglow42 <30480333+Alpenglow42@users.noreply.github.com>
2021-08-27 19:30:27 +0700

    Create vocab_list.py

---
+++ vocab_list.py
@@ -0,0 +1,178 @@
+
+
+
+
+
+vocab_list = [
+
+  {
+  "word": "Ambigous",
+  "definition":"Having Several meanings  unclear and difficult to understand",
+  "sentence": "When I asked the HR manager what my chances, were of getting the job, she gave me a very ambiguous reply."
+
+
+  },
+
+  {
+  "word": "Antagonize",
+  "definition": "To tease or be hostile towards a person or group.",
+  "sentence": "The boy loves to antagonize his little sister by pulling her hair.",
+  },
+
+  {
+  "word": "Brusque",
+  "definition": "Abrupt to the point of rudeness.",
+  "sentence": "After being away for so long, I expected more than her brusque greeting",
+  },
+
+  {
+  "word": "Coalesce",
+  "definition": "To combine or grow together.",
+  "sentence": "The people on the street eventually coalesced into a group.",
+  },
+
+  {
+  "word": "Copious",
+  "definition": "abundant in supply or quantity.",
+  "sentence": " He always takes copious notes during class to study later on.",
+  },
+
+  {
+  "word": "Deviate",
+  "definition": "To differ from the norm.",
+  "sentence": "I decided to deviate from my normal route home and took a shortcut.",
+  },
+
+
+  {
+  "word": "Decipher",
+  "definition": "To find the meaning of",
+  "sentence": "The spy deciphered the secret code.",
+  },
+
+
+  {
+  "word": "Eloquent",
+  "definition": "fluent or persuasive in speaking or writing. adj;clearly expressing or indicating something. ",
+  "sentence": "His speeches in the chamber were always eloquent and powerful.",
+  },
+
+  {
+  "word": "Erroneous",
+  "definition": "wrong; incorrect.",
+  "sentence": "He apologized for his erroneous statement.",
+  },
+
+  {
+  "word": "Feasible",
+  "definition": "possible to do easily or conveniently.",
+  "sentence": "This study plan you made for me sounds feasible, even with my work schedule.",
+  },
+
+  {
+  "word": "Haphazard",
+  "definition": "Lacking planning",
+  "sentence": "There was no schedule, so the event was very haphazard.",
+  },
+
+  {
+  "word": "Hindrance",
+  "definition": "Something that causes delay or resistance.",
+  "sentence": "Her hatred of public transportation is a hindrance when trying to get around New York City.",
+  },
+
+  {
+  "word": "Indisputable",
+  "definition": "Not able to be challenged.",
+  "sentence": "She’s the indisputable star of the basketball team.",
+  },
+
+  {
+  "word": "Intricate",
+  "definition": "Highly detailed",
+  "sentence": "The pattern on this blanket is so intricate.",
+  },
+
+  {
+  "word": "Lampoon",
+  "definition": "To mock or ridicule.",
+  "sentence": "The cartoonist lampooned the president’s speech.",
+  },
+
+  {
+  "word": "Morose",
+  "definition": "Gloomy, depressed.",
+  "sentence": "The boy was morose after hearing he didn’t make the football team.",
+  },
+
+  {
+  "word": "Negligent",
+  "definition": "Lazy, neglectful",
+  "sentence": "The negligent babysitter invited her friends over while the children were upstairs.",
+  },
+
+  {
+  "word": "Parsimonious",
+  "definition": "Frugal, stingy.",
+  "sentence": "The parsimonious woman only donated a dollar to charity.",
+  },
+
+
+  {
+  "word": "Prestigious",
+  "definition": "Having a high reputation.",
+  "sentence": "Harvard is one of the most prestigious colleges in the United States.",
+  },
+
+  {
+  "word": "Recapitulate",
+  "definition": "To give a brief summary.",
+  "sentence": "At the end of his speech, the politician made sure to recapitulate his main points",
+  },
+
+   {
+  "word": "Rhetorical",
+  "definition": "Used just for style or impact.",
+  "sentence": "You aren’t expected to actually answer rhetorical questions.",
+  },
+
+
+  {
+  "word": "Scrutinize",
+  "definition": "To very carefully examine.",
+  "sentence": "The judges were looking for perfection, so they scrutinized every entry.",
+  },
+
+  {
+  "word": "Stagnant",
+  "definition": "Sluggish, showing little movement.",
+  "sentence": "With few new jobs created, the economy has remained stagnant for the past five years.",
+  },
+
+   {
+  "word": "Tuition",
+  "definition": "The fee for instruction at a school.",
+  "sentence": "College tuition prices have gone up in recent decades",
+  },
+
+
+  {
+  "word": "Vacillate",
+  "definition": "To waver or be indecisive.",
+  "sentence": "She vacillated between the two dresses before finally deciding to wear a skirt.",
+  },
+
+  # {
+  # "word": "",
+  # "definition": "",
+  # "sentence": "",
+  # },
+
+
+  
+ 
+
+
+
+
+]
